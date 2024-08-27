c14cb43e82fa31ae71df57ad9cb1683436aa6f0d
Dine5hk <99193767+Dine5hk@users.noreply.github.com>
2024-08-27 19:44:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -50,6 +50,12 @@ resource "aws_security_group" "terraform" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allows all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "app_server" {
