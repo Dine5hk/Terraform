d0047b0605f4f33b2d024acb0093d04dadb2ee6a
Dine5hk <99193767+Dine5hk@users.noreply.github.com>
2024-09-07 13:59:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,6 +23,40 @@ variable "medium" {
     default = "t2.medium"
 }
 
+
+variable "prometheus" {
+  description = "Script to install Prometheus and its components"
+  default = <<EOF
+#!/bin/bash
+mkdir -p /home/ubuntu/prometheus
+cd /home/ubuntu/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.54.1/prometheus-2.54.1.linux-amd64.tar.gz
+wget https://github.com/prometheus/alertmanager/releases/download/v0.27.0/alertmanager-0.27.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.25.0/blackbox_exporter-0.25.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar -xvf prometheus-2.54.1.linux-amd64.tar.gz
+tar -xvf alertmanager-0.27.0.linux-amd64.tar.gz
+tar -xvf blackbox_exporter-0.25.0.linux-amd64.tar.gz
+tar -xvf node_exporter-1.8.2.linux-amd64.tar.gz
+mv prometheus-2.54.1.linux-amd64 prometheus
+mv alertmanager-0.27.0.linux-amd64 alertmanager
+mv blackbox_exporter-0.25.0.linux-amd64 blackbox_exporter
+mv node_exporter-1.8.2.linux-amd64 node_exporter
+rm *.tar.gz
+EOF
+}
+
+
+# Define a variable for the Grafana setup script
+variable "grafana" {
+  description = "Script to pull and run Grafana Docker container"
+  default = <<EOF
+docker pull grafana/grafana
+docker run -d -p 3000:3000 --name=grafana -v grafana-data:/var/lib/grafana grafana/grafana
+EOF
+}
+
+
 # funcation called 
 
 resource "aws_instance" "app_server" {
